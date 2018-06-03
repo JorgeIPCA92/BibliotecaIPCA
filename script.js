70f9b1e8804348d02bafdcbb28aa23d8aed6444e
Andrea Pacheco Veronese <>
2018-06-04 00:34:33 +0100

    Ya es responsive menos el footer que apesta....

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $('.contactos').on('click', function(){
   $('.lista-cont').fadeToggle()  
 })
+
+$('.contactos2').on('click', function(){
+  $('.lista-cont2').fadeToggle()  
+})
+
+$('.bottom-menu').on('click', function(){
+    $('.menu2,.real-menu').toggleClass('real-menu');
+})
